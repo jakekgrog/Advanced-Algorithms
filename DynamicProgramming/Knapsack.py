87d2abf735e88a444feacb05269bfb9c104c1fef
Jake Grogan <Jake.kgrogan@gmail.com>
2018-10-27 21:17:29 +0100

    Create Knapsack.py

---
+++ DynamicProgramming/Knapsack.py
@@ -0,0 +1,24 @@
+def knapsack(capacity, items, memo):
+    if capacity == 0:
+        return 0
+    else:
+        if capacity in memo:
+            return memo[capacity]
+        else:
+            options = [item[0] + knapsack(capacity-item[1], items, memo) for item in items if item[1] <= capacity]
+            if len(options) == 0:
+                return 0
+            else:
+                memo[capacity] = max(options)
+                return memo[capacity]
+
+def main():
+    # A single item is a value, weight tuple
+    items = [(20, 1), (300, 2)]
+    capacity = 401
+    memo = {}
+    ks = knapsack(capacity, items, memo)
+    print(ks)
+
+if __name__ == "__main__":
+    main()
